373b52d1e3c4892f17bddaf6a80a6c8aab458061
ashokminfo03 <ashok.minfo03@gmail.com>
2025-11-26 12:58:18 +0530

    remove dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
 import "./globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter"
